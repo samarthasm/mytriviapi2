598a38478083051898a0821ae4cf19d0235034b9
Samartha S M <Samartha.SM@shell.com>
2022-07-12 11:13:46 +0530

    Adding minor comment changes to init

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
- 
+
 from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
